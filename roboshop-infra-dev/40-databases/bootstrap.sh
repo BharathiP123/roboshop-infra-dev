dc0083b002bd266af9d11477234ab6d81ca02aa6
bharathip123 <bharathip0528@gmail.com>
2025-10-30 13:48:10 -0600

    updatedthe files

--- roboshop-infra-dev/40-databases/bootstrap.sh
+++ roboshop-infra-dev/40-databases/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 component=$1
-environment=$2
+#environment=$2
 dnf install ansible -y
-ansible-pull -U https://github.com/BharathiP123/-ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/BharathiP123/-ansible-roboshop-roles-tf.git -e component=$component main.yaml
