df8b34846823fae4e6a5bed8044b4fad9ca32e0e
bharathip123 <bharathip0528@gmail.com>
2025-11-01 15:31:09 -0600

    updated the files

--- roboshop-infra-dev/60-catalogue/locals.tf
+++ roboshop-infra-dev/60-catalogue/locals.tf
@@ -2,6 +2,7 @@ locals {
   common_name_suffix = "${var.projectname}-${var.environment}"
   #vpc_id = data.aws_ssm_parameter.vpc_id.value
   ami_id = data.aws_ami.myami.id
+  zone_id = data.aws_route53_zone.zone_id.id
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   common_tags = {
