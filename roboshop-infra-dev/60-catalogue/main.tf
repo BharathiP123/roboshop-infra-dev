784da1dfd787d88c55a24bbf116941660de7a7b9
bharathip123 <bharathip0528@gmail.com>
2025-11-03 13:10:22 -0700

    updated the files

--- roboshop-infra-dev/60-catalogue/main.tf
+++ roboshop-infra-dev/60-catalogue/main.tf
@@ -172,8 +172,8 @@ resource "aws_autoscaling_group" "catalogue" {
     )
   
     content {
-        key                 = "tag.key"
-        value               = "tag.value"
+        key                 = tag.key
+        value               = tag.value
         propagate_at_launch = true
         
         }
