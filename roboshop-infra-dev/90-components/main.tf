02fcbf78c397d1e79a730dbeaf95a927eed1fda9
bharathip123 <bharathip0528@gmail.com>
2025-11-14 11:12:19 -0700

    updated the file

--- roboshop-infra-dev/90-components/main.tf
+++ roboshop-infra-dev/90-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
     for_each = var.components
-    source = "https://github.com/BharathiP123/terraform-roboshop-component.git?ref=main"
+    source = "git::https://github.com/BharathiP123/terraform-roboshop-component.git?ref=main"
     compnent = each.key
     rule_priority = each.value.rule_priority
       
